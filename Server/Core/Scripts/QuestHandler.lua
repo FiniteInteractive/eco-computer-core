a463615d65dab56028c9ef6f921e014ebff8b2e6
just a bit clumsy! <fourybfb@outlook.com>
2022-05-25 23:56:10 -0700

    Better Quest Handling
    code drastically improved, looping through tables

--- Server/Core/Scripts/QuestHandler.lua
+++ Server/Core/Scripts/QuestHandler.lua
@@ -1,60 +1,79 @@
 local QuestModule = require(game.ServerStorage.Modules.QuestModule)
 local QuestTemplate = game.ServerStorage.UiElements.QuestTemplate
 local dataStore = game:GetService("DataStoreService")
-local ds1 = dataStore:GetDataStore("EcoCC_QuestsSystem")
+local QuestData = dataStore:GetDataStore("EcoCCQuestStore")
 local QuestEvent = game.ReplicatedStorage.RemotesEvents.Quests:WaitForChild("QuestDone")
 
--- switching to BoolValue
 
--- i could find one or two ways to improve this basic code
-game.Players.PlayerAdded:Connect(function(player)
-	local leaderstats = player:WaitForChild("leaderstats",15)
+-- Building from scratch again...
+-- Part of code forked from Tech with Mike
+
+local function onPlayerJoin(player)  -- Runs when players join
+
+	local leaderstats = player:WaitForChild("leaderstats",9)
 	local QuestsFolder = Instance.new("Folder")
 	QuestsFolder.Name = "Quests"
 	QuestsFolder.Parent = player
-	-- after this point
+
 	local IgnitionRctr2 = Instance.new("BoolValue")
 	IgnitionRctr2.Name = "IgnitionRctr2"
 	IgnitionRctr2.Parent = QuestsFolder
-	IgnitionRctr2.Value = ds1:SetAsync(player.UserId) or false
 	local Coins = Instance.new("BoolValue")
 	Coins.Name = "Coins"
 	Coins.Parent = QuestsFolder
-	Coins.Value = ds1:SetAsync(player.UserId) or false
 	local Vent = Instance.new("BoolValue")
 	Vent.Name = "Vent"
 	Vent.Parent = QuestsFolder
-	Vent.Value = ds1:SetAsync(player.UserId) or false
 	local FriendsJoin = Instance.new("BoolValue")
 	FriendsJoin.Name = "FriendsJoin"
 	FriendsJoin.Parent = QuestsFolder
-	FriendsJoin.Value = ds1:SetAsync(player.UserId) or false
 	local SecretMarket = Instance.new("BoolValue")
 	SecretMarket.Name = "SecretMarket"
 	SecretMarket.Parent = QuestsFolder
-	SecretMarket.Value = ds1:SetAsync(player.UserId) or false
 	local WindMachine = Instance.new("BoolValue")
 	WindMachine.Name = "WindMachine"
 	WindMachine.Parent = QuestsFolder
-	WindMachine.Value = ds1:SetAsync(player.UserId) or false
 	local StartReactors = Instance.new("BoolValue")
 	StartReactors.Name = "StartReactors"
 	StartReactors.Parent = QuestsFolder
-	StartReactors.Value = ds1:SetAsync(player.UserId) or false
 	local ShutdownReactors = Instance.new("BoolValue")
 	ShutdownReactors.Name = "ShutdownReactors"
 	ShutdownReactors.Parent = QuestsFolder
-	ShutdownReactors.Value = ds1:SetAsync(player.UserId) or false
 	local GetIntoMachineYuh = Instance.new("BoolValue")
 	GetIntoMachineYuh.Name = "GetIntoMachineYuh"
 	GetIntoMachineYuh.Parent = QuestsFolder
-	GetIntoMachineYuh.Value = ds1:SetAsync(player.UserId) or false
 	local Phantoms = Instance.new("BoolValue")
 	Phantoms.Name = "PhntmPickGet"
 	Phantoms.Parent = QuestsFolder
-	Phantoms.Value = ds1:SetAsync(player.UserId) or false
-	-- end --
-	local eco = leaderstats.Eco
+
+
+	local playerUserId = "Player_" .. player.UserId  --Gets player ID
+	local data = QuestData:GetAsync(playerUserId)  --Checks if player has stored data
+
+	if data then
+		Coins.Value = data['Coins']
+		Vent.Value = data['Vent']
+		FriendsJoin.Value = data['FriendsJoin']
+		SecretMarket.Value = data['SecretMarket']
+		WindMachine.Value = data['WindMachine']
+		StartReactors.Value = data['StartReactors']	
+		ShutdownReactors.Value = data['ShutdownReactors']
+		GetIntoMachineYuh.Value = data['GetIntoMachineYuh']
+		Phantoms.Value = data['PhntmPickGet']
+	else
+		-- Data store is working, but no current data for this player
+		Coins.Value = false
+		Vent.Value = false
+		FriendsJoin.Value = false
+		SecretMarket.Value = false
+		WindMachine.Value = false
+		StartReactors.Value = false
+		ShutdownReactors.Value = false
+		GetIntoMachineYuh.Value = false
+		Phantoms.Value = false
+	end
+
+	local eco = leaderstats:WaitForChild("Eco",9)
 	for I=1,8 do
 		local clone = QuestTemplate:Clone()
 		local Title = ""
@@ -64,17 +83,17 @@ game.Players.PlayerAdded:Connect(function(player)
 		if I == 1 then
 			Reward = 15
 			Title = "Get 100 Eco."
-			Text = tostring("Quest Completed. Heres "..Reward.." Eco! If you have already completed this quest, you won't get a reward!")
+			Text = tostring("Quest Completed. You have earned "..Reward.." Eco!")
 			Icon = ""
-			if Coins.Value == false and player.leaderstats.Eco.Value >= 100 then
+			if Coins.Value == false  then
 				QuestModule.MonitorCurrency(player,100,Reward,{Title,Text,Icon})
 			elseif Coins.Value == true then
 				return "Is Already Done"
 			end
 		elseif I == 2 then
 			Reward = 25
-			Title = "Get blown away using the Wind Machine"
-			Text = tostring("Quest Completed. Heres "..Reward.." Eco! If you have already completed this quest, you won't get a reward!")
+			Title = "Get blown away in Wind Machine"
+			Text = tostring("Quest Completed. You have earned "..Reward.." Eco!")
 			Icon = ""
 			if WindMachine.Value == false then
 				QuestModule.FindPart(player,workspace.World.Objects.Miscellaneous.WindPlatform,Reward,{Title,Text,Icon})
@@ -84,23 +103,18 @@ game.Players.PlayerAdded:Connect(function(player)
 		elseif I == 3 then
 			Reward = 125
 			Title = "Prepare Ignition for Reactor 2"
-			Text = tostring("Quest Completed. Heres "..Reward.." Eco! If you have already completed this quest, you won't get a reward!")
+			Text = tostring("Quest Completed. You have earned "..Reward.." Eco!")
 			Icon = ""
 			if IgnitionRctr2.Value == false then
 				QuestModule.Switch(player,workspace.World.Objects.Facility.Reactor2.ControlRoom.Interactables.Switchables.IgnitePrep,Reward,{Title,Text,Icon})
 			elseif IgnitionRctr2.Value == true then
 				return "Is Already Done"
 			end
-			--elseif I == 4 then
-			--	Reward = 50
-			--	Title = "Combine Ores"
-			--	Text = tostring("Quest Completed. Heres "..Reward.." Eco!")
-			--	Icon = ""
-			--QuestModule.Switch(player,workspace.IgnitePrep,Reward,{Title,Text,Icon})
+
 		elseif I == 4 then
 			Reward = 250
 			Title = "Get a Phantom Pick"
-			Text = tostring("Quest Completed. Heres "..Reward.." Eco! If you have already completed this quest, you won't get a reward!")
+			Text = tostring("Quest Completed. You have earned "..Reward.." Eco!")
 			Icon = ""
 			if Phantoms.Value == false then
 				QuestModule.MonitorPhantom(player,Reward,{Title,Text,Icon})
@@ -110,29 +124,29 @@ game.Players.PlayerAdded:Connect(function(player)
 		elseif I == 5 then
 			Reward = 35
 			Title = "Get into the Mutation Machine in Research Lab"
-			Text = tostring("Quest Completed. Heres "..Reward.." Eco! If you have already completed this quest, you won't get a reward!")
+			Text = tostring("Quest Completed. You have earned "..Reward.." Eco!")
 			Icon = ""
-			if GetIntoMachineYuh.Value == 0 then
+			if GetIntoMachineYuh.Value == false then
 				QuestModule.FindPart(player,workspace.World.Objects.Miscellaneous.MutationMachIn,Reward,{Title,Text,Icon})
-			elseif GetIntoMachineYuh.Value == 1 then
+			elseif GetIntoMachineYuh.Value == true then
 				return "Is Already Done"
 			end
-			
+
 		elseif I == 6 then
 			Reward = 15
 			Title = "Vent!"
-			Text = tostring("Quest Completed. Heres "..Reward.." Eco! If you have already completed this quest, you won't get a reward!")
+			Text = tostring("Quest Completed. You have earned "..Reward.." Eco!")
 			Icon = ""
 			if Vent.Value == false then
 				QuestModule.FindPart(player,workspace.World.Objects.Miscellaneous.VentPlatform,Reward,{Title,Text,Icon})
 			elseif Vent.Value == true then
 				return "Is Already Done"
 			end
-			
+
 		elseif I == 7 then
 			Reward = 100
 			Title = "Get an item from the Secret Market in Vent"
-			Text = tostring("Quest Completed. Heres "..Reward.." Eco! If you have already completed this quest, you won't get a reward!")
+			Text = tostring("Quest Completed. You have earned "..Reward.." Eco!")
 			Icon = ""
 			if SecretMarket.Value == false then
 				QuestModule.MonitorTools(player,Reward,{Title,Text,Icon})
@@ -142,32 +156,61 @@ game.Players.PlayerAdded:Connect(function(player)
 		else
 			Reward = 499
 			Title = "Have a friend join."
-			Text = tostring("Quest Completed. Heres "..Reward.." Eco! If you have already completed this quest, you won't get a reward!")
+			Text = tostring("Quest Completed. You have earned "..Reward.." Eco!")
 			Icon = ""
 			QuestModule.MonitorFriends(player,Reward,{Title,Text,Icon})
 		end
 		clone.Title.Text = Title
 		clone.Reward.Text = tostring(Reward.." Eco")
-		clone.Parent = player.PlayerGui:WaitForChild("CoreGUI").Quests.QuestUI.QuestMenu.QuestsList
+		clone.Parent = player.PlayerGui:WaitForChild("CoreGUI",6).Quests.QuestUI.QuestMenu.QuestsList
 		task.wait(0.05)
 	end
-	player.Quests.Changed:Connect(function()
+
+	-- first check
 	for _,v in pairs(player.Quests:GetChildren()) do
 		if v:IsA("BoolValue") then
-			if v == true then
+			QuestData:SetAsync(player.UserId, v.Value)
+			if v.Value == true then
+				print(tostring(v).." may be done")
 				QuestEvent:FireClient(player,v.Name)
 			end
 		end
 	end
-end)
 
-player.Quests.Changed:Connect(function()
-	for _,v in pairs(player.Quests:GetChildren()) do
-		if v:IsA("BoolValue") then
-			ds1:SetAsync(player.UserId)
+	-- continous check
+	player.Quests.Changed:Connect(function()
+		for _,v in pairs(player.Quests:GetChildren()) do
+			if v:IsA("BoolValue") then
+				QuestData:SetAsync(player.UserId, v.Value)
+				if v.Value == true then
+					print(tostring(v).." may be done")
+					QuestEvent:FireClient(player,v.Name)
+				end
+			end
 		end
+	end)
+end
+
+local function create_table(player)
+	local player_stats = {}
+	for _, stat in pairs(player.leaderstats:GetChildren()) do
+		player_stats[stat.Name] = stat.Value
 	end
-end)
-end)
+	return player_stats
+end
 
+local function onPlayerExit(player)  --Runs when players exit
 
+	local player_stats = create_table(player)
+	local success, err = pcall(function()
+		local playerUserId = "Player_" .. player.UserId
+		QuestData:SetAsync(playerUserId, player_stats) --Saves player data
+	end)
+
+	if not success then
+		warn('Could not save data for player ' .. player.Name .. ': ' .. 'because' .. err)
+	end
+end
+
+game.Players.PlayerAdded:Connect(onPlayerJoin)
+game.Players.PlayerRemoving:Connect(onPlayerExit)
